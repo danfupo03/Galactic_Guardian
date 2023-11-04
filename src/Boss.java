50d2f275071f640594d6c4079be48a5d8f909ddf
danfupo03 <danfupo@outlook.com>
2023-11-04 00:08:05 -0600

    feat(Boss): Add the boss sprite and boss class

---
+++ src/Boss.java
@@ -0,0 +1,58 @@
+import java.awt.image.BufferedImage;
+import java.awt.Image;
+import java.awt.Graphics2D;
+import javax.swing.ImageIcon;
+import java.awt.Rectangle;
+
+public class Boss extends Sprite {
+    private static final int B_HEIGHT = 285;
+    private static final int BOSS_HEIGHT = 25;
+
+    private int yDirection = 1;
+
+    private int originalWidth, originalHeight;
+
+    public Boss(int x, int y) {
+        super(x, y);
+    }
+
+    public void loadImage() {
+        ImageIcon ii = new ImageIcon("assets/images/boss.png");
+        Image originalImage = ii.getImage();
+
+        originalWidth = originalImage.getWidth(null);
+        originalHeight = originalImage.getHeight(null);
+
+        BufferedImage resizedImage = new BufferedImage(25, 25, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2d = resizedImage.createGraphics();
+        g2d.drawImage(originalImage, 0, 0, 25, 25, null);
+        g2d.dispose();
+
+        image = resizedImage;
+
+        width = image.getWidth(null);
+        height = image.getHeight(null);
+    }
+
+    public void move() {
+        y += yDirection;
+
+        if (y <= 0 || y >= B_HEIGHT - BOSS_HEIGHT) {
+            yDirection *= -1;
+        }
+
+        System.out.println(y);
+    }
+
+    public Rectangle getResizedBounds() {
+        return new Rectangle(x, y, width, height);
+    }
+
+    public int getOriginalWidth() {
+        return originalWidth;
+    }
+
+    public int getOriginalHeight() {
+        return originalHeight;
+    }
+}
